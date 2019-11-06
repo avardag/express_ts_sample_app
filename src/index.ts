a9a44193efc00c812791776b7ad527b507e2c854
Dagestan05 <steklofan@gmail.com>
2019-11-06 22:40:02 +0100

    made simple app with routes w/o using much TS

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express, {Response, Request} from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session'
 
 const app = express();
 const port= process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response)=>{
-  res.send("<h1>Hello from here</h1>")
-})
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieSession({ keys: ['akjdjldjlj'] }))
 
+//Routes
+app.use(router);
 
 app.listen(port, ()=>{
   console.log(`server running on port ${port}`);
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,90 @@
+import {Router, Request, Response, NextFunction} from 'express';
+
+const router: Router = Router()
+
+interface RequestWithBody extends Request{
+  body: {
+    //keys og this obj are strings, value of that key is either a string or undefined
+    [key: string]: string | undefined
+  }
+}
+//Middleware
+function isAuth (req: Request, res: Response, next: NextFunction): void{
+  if(req.session && req.session.loggedIn){
+    next();
+    return;
+  }
+  res.status(403);
+  res.send(`
+  <h3>Not allowed</h3>
+  <a href="/">Back to home page</a>
+  `)
+}
+
+router.get('/', (req: Request, res: Response)=>{
+  //check req.session
+  if(req.session && req.session.loggedIn){
+    res.send(`
+    <div>
+      <h3>Hello. You are logged in</h3>
+      <div>
+      <a href="/logout">Logout</a>
+      </div>
+    </div>
+    `)
+  }else{
+    res.send(`
+    <div>
+      <h3>Hi. You are not logged in</h3>
+      <div>
+      <a href="/login">Login</a>
+      </div>
+    </div>
+    `)
+  }
+  res.send("<h1>Hello from here</h1>")
+})
+
+router.get('/login', (req: Request, res: Response)=>{
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" type="email"/>
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password"/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  `)
+})
+
+router.post('/login', (req: RequestWithBody, res: Response)=>{
+  const {email, password} = req.body;
+
+  //type guard
+  if(email && password && email === 'alex@gmail.com' && password === '123'){
+    //user logged in. save in session
+    req.session = {
+      loggedIn: true
+    }
+    // redirect to root route
+    res.redirect('/')
+  }else{
+    res.send("You must provide an email and a password")
+  }
+})
+
+router.get('/logout', (req: Request, res: Response)=>{
+  req.session = undefined;
+  res.redirect('/')
+})
+////////////
+// protected routes
+/////
+router.get('/protected', isAuth,  (req: Request, res: Response)=>{
+  res.send("Welcome to the team. This is a protected route")
+})
+export {router};
